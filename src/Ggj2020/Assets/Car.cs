32b03691907436ffb0b47a546dcd9a35ed23af9e
Anton Herzog <info@antonherzog.com>
2020-01-31 22:03:52 +0100

    Kind of working rotate via keyboard.

--- src/Ggj2020/Assets/Car.cs
+++ src/Ggj2020/Assets/Car.cs
@@ -6,32 +6,94 @@ using Random = System.Random;
 
 public class Car
 {
-    private readonly Vector3 _stearingVector = new Vector3(0, 5, 0);
-    public CarData Data { get; } = new CarData();
-    public int PlayerId => Data.PlayerId;
-    public Vector3 Position => Data.Position;
-    public Quaternion Rotation => Data.Rotation;
+	private readonly Vector3 _stearingVector = new Vector3(0, 1.5f, 0);
+	private CarData Data { get; } = new CarData();
+	public int PlayerId => Data.PlayerId;
+	public Vector3 Position => Data.Position;
+	public Vector3 Rotation => Data.Rotation;
 
-    public void StearLeft()
-    {
-        Data.Rotation.SetFromToRotation(new Vector3(0, 0, 0), _stearingVector);
-    }
+	public void StartStearLeft()
+	{
+		Data.Stearing = CarStearing.Left;
+	}
 
-    public void StearRight()
-    {
-        Data.Rotation.SetFromToRotation(new Vector3(0, 0, 0), -_stearingVector);
-    }
+	public void StartStearRight()
+	{
+		Data.Stearing = CarStearing.Right;
+	}
 
-    public void UpdatePosition(Vector3 newPosition)
-    {
-        Data.Position = newPosition;
-    }
+	public void StopStearLeft()
+	{
+		InvalidateStearing(CarStearing.Left);
+	}
+
+	public void StopStearRight()
+	{
+		InvalidateStearing(CarStearing.Right);
+	}
+
+	private void InvalidateStearing(CarStearing carStearing)
+	{
+		if (Data.Stearing == carStearing)
+		{
+			Data.Stearing = CarStearing.None;
+		}
+	}
+
+	private void Rotate(Vector3 stearingVector)
+	{
+		var angles = Data.Rotation;
+		var newAngles = angles + stearingVector;
+		Data.Rotation = newAngles;
+	}
+
+
+	public void UpdatePosition(Vector3 newPosition)
+	{
+		Data.Position = newPosition;
+	}
+
+	public void UpdateRotation(Quaternion transformRotation)
+	{
+		Data.Rotation = transformRotation.eulerAngles;
+	}
+
+	public void Tick()
+	{
+		switch (Data.Stearing)
+		{
+			case CarStearing.Left:
+				Rotate(-_stearingVector);
+				break;
+			case CarStearing.Right:
+				Rotate(_stearingVector);
+				break;
+			case CarStearing.None:
+				break;
+		}
+	}
+}
+
+public enum CarStearing
+{
+	None,
+	Left,
+	Right
+}
+
+public enum CarAcceleration
+{
+	None,
+	Forward,
+	Backward
 }
 
 public class CarData
 {
-    private static readonly Random _rng = new Random();
-    public int PlayerId { get; } = _rng.Next();
-    public Vector3 Position { get; set; } = new Vector3();
-    public Quaternion Rotation { get; } = new Quaternion();
+	private static readonly Random _rng = new Random();
+	public int PlayerId { get; } = _rng.Next();
+	public Vector3 Position { get; set; }
+	public Vector3 Rotation { get; set; }
+	public CarAcceleration Acceleration { get; set; }
+	public CarStearing Stearing { get; set; }
 }
---
+++ src/Ggj2020/Assets/MoveLikeCar.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+using static UnityEngine.KeyCode;
+
+public class MoveLikeCar : MonoBehaviour
+{
+	private Car State = new Car();
+
+	private Rigidbody2D _body;
+
+	// Start is called before the first frame update
+	private void Start()
+	{
+		_body = gameObject.GetComponent<Rigidbody2D>();
+		State.UpdatePosition(gameObject.transform.position);
+		State.UpdateRotation(gameObject.transform.rotation);
+	}
+
+	private bool Down(KeyCode k) => Input.GetKeyDown(k);
+	private bool Up(KeyCode k) => Input.GetKeyUp(k);
+
+	// Update is called once per frame
+	private void Update()
+	{
+		if (Down(RightArrow))
+		{
+			State.StartStearRight();
+		}
+
+		if (Up(RightArrow))
+		{
+			State.StopStearRight();
+		}
+
+		if (Down(LeftArrow))
+		{
+			State.StartStearLeft();
+		}
+
+		if (Up(LeftArrow))
+		{
+			State.StopStearLeft();
+		}
+
+		State.Tick();
+
+		gameObject.transform.position = State.Position;
+		gameObject.transform.rotation = Quaternion.Euler(State.Rotation);
+	}
+}
