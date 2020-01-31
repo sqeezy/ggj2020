dac00973589923072d1cef08f91c94dcde797e70
Anton Herzog <info@antonherzog.com>
2020-01-31 20:52:12 +0100

    Add car. First draft

---
+++ src/Ggj2020/Assets/Car.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Data.Common;
+using UnityEngine;
+using Random = System.Random;
+
+public class Car
+{
+    private Vector3 _stearingVector = new Vector3(0, 5, 0);
+    public CarData Data { get; } = new CarData();
+    public int PlayerId => Data.PlayerId;
+    public Vector3 Position => Data.Position;
+    public Quaternion Rotation => Data.Rotation;
+
+    public void StearLeft()
+    {
+        Data.Rotation.SetFromToRotation(new Vector3(0, 0, 0), _stearingVector);
+    }
+
+    public void StearRight()
+    {
+        Data.Rotation.SetFromToRotation(new Vector3(0, 0, 0), -_stearingVector);
+    }
+}
+
+public class CarData
+{
+    private static readonly Random _rng = new Random();
+    public int PlayerId { get; } = _rng.Next();
+    public Vector3 Position { get; } = new Vector3();
+    public Quaternion Rotation { get; } = new Quaternion();
+}
