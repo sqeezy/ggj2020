69de4e076818030edeff77e3b2f38135d78505a9
Napster <napster@npstr.space>
2020-02-01 14:26:58 +0100

    Fix imports

--- src/Ggj2020/Assets/Scripts/Network/WebSocketService.cs
+++ src/Ggj2020/Assets/Scripts/Network/WebSocketService.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Text;
 using UnityEngine;
 using HybridWebSocket;
-using UnityEditor.PackageManager;
 using Zenject;
 
 /// <summary>
