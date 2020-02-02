7aa900036882c5dddbebd315df2abdb34a6f8377
Anton Herzog <info@antonherzog.com>
2020-02-02 01:08:15 +0100

    Add follow first camera logic

--- src/Ggj2020/Assets/Scripts/CameraSystem/FollowTarget.cs
+++ src/Ggj2020/Assets/Scripts/CameraSystem/FollowTarget.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
 using UnityEngine;
 using Zenject;
 
@@ -22,12 +23,42 @@ public class FollowTarget : MonoBehaviour
 	// Update is called once per frame
 	private void LateUpdate()
 	{
+		FollowFirst();
+	}
+
+	private void FollowAllOtherThanLast()
+	{
+		var orderedPlayers = _model.GetOrderedPlayers().ToArray();
+		if (!orderedPlayers.Any())
+		{
+			return;
+		}
+
+		var first = orderedPlayers.FirstOrDefault();
+		var secondToLast = orderedPlayers.Skip(orderedPlayers.Length - 2).FirstOrDefault();
+
+		var y1 = first?.PlayerData.CarData.Position.y ?? CameraPositon.y;
+		var y2 = secondToLast?.PlayerData.CarData.Position.y ?? CameraPositon.y;
+
+		var mid = (y1 + y2) / 2;
+		UpdateCameraY(mid);
+	}
+
+	private void FollowFirst()
+	{
 		var first = _model.GetOrderedPlayers().FirstOrDefault();
 		if (first != null)
 		{
 			var position = first.PlayerData.CarData.Position;
-			var transformPosition = gameObject.transform.position;
-			gameObject.transform.position = new Vector3(transformPosition.x, position.y - 5, transformPosition.z);
+			var positionY = position.y;
+			UpdateCameraY(positionY);
 		}
 	}
+
+	private void UpdateCameraY(float positionY)
+	{
+		gameObject.transform.position = new Vector3(CameraPositon.x, positionY - 5, CameraPositon.z);
+	}
+
+	private Vector3 CameraPositon => gameObject.transform.position;
 }
