9f524fd4aa6b0d8a31361d1e7a53c2721f3b0081
Napster <napster@npstr.space>
2020-02-02 03:09:10 +0100

    Main UI can go to master view

---
+++ src/Ggj2020/Assets/Scripts/GameState/DriverUIState.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DriverUIState : IGameState
+{
+	private readonly IAssetService _assetService;
+	private GameObject _menu;
+
+	public DriverUIState(IAssetService assetService)
+	{
+		_assetService = assetService;
+	}
+	public IEnumerator Load()
+	{
+		yield return _assetService.LoadAsset(AssetCatalogue.DriverUI);
+	}
+
+	public IEnumerator Enter()
+	{
+		_menu = _assetService.GetAssetInstance(AssetCatalogue.DriverUI);
+		_menu.SetActive(true);
+		yield return null;
+	}
+
+	public IEnumerator Exit()
+	{
+		Object.Destroy(_menu);
+		yield return null; 
+	}
+
+	public IEnumerator Unload()
+	{
+		yield return _assetService.UnloadAsset(AssetCatalogue.DriverUI);
+	}
+}
