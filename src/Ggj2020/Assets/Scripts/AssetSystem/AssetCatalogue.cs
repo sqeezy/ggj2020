02a1e94707e4f258b82c3706df0a0fdc08150ea0
Friedrich Wessel <friedrich.wessel@innogames.com>
2020-02-02 15:39:44 +0100

    use map 2

--- src/Ggj2020/Assets/Scripts/AssetSystem/AssetCatalogue.cs
+++ src/Ggj2020/Assets/Scripts/AssetSystem/AssetCatalogue.cs
@@ -6,7 +6,7 @@ public static class AssetCatalogue
 {
 	public static string Car01 = "Content/Car/Car01";
 	public static string StartMenu = "Content/UI/StartMenu";
-	public static string Map01 = "Content/Maps/Map01";
+	public static string Map01 = "Content/Maps/Map02";
 	public static string DriverUI = "Content/UI/DriverUI";
 	public static string Projectile = "Content/Weapons/Projectile";
 }
