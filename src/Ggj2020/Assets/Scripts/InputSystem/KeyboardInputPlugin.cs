d724092356a85df472b450b259abdd0d39083901
Friedrich Wessel <friedrich.wessel@innogames.com>
2020-02-05 21:02:26 +0100

    Basic dynamic screen

--- src/Ggj2020/Assets/Scripts/InputSystem/KeyboardInputPlugin.cs
+++ src/Ggj2020/Assets/Scripts/InputSystem/KeyboardInputPlugin.cs
@@ -1,25 +1,41 @@
-﻿using InputSystem;
+﻿using System.Collections.Generic;
+using InputSystem;
+using UnityEditor;
 using UnityEngine;
 
 public class KeyboardInputPlugin : IInputPlugin
 {
-	private readonly PlayerKeyboardInputMapper _playerZero;
-	private readonly PlayerKeyboardInputMapper _playerOne;
+	private List<PlayerKeyboardInputMapper> _registeredPlayers = new List<PlayerKeyboardInputMapper>();
+	
+	private IInputDispatcher _inputDispatcher;
 
 	public KeyboardInputPlugin(IInputDispatcher inputDispatcher)
 	{
-		_playerZero = new PlayerKeyboardInputMapper("0", KeyCode.LeftArrow,
+		_inputDispatcher = inputDispatcher;
+		_registeredPlayers.Add(new PlayerKeyboardInputMapper("0", KeyCode.LeftArrow,
 			KeyCode.RightArrow, KeyCode.UpArrow, KeyCode.DownArrow, KeyCode.RightShift, KeyCode.Return,
-			inputDispatcher);
+			inputDispatcher));
 
-		_playerOne = new PlayerKeyboardInputMapper("1", KeyCode.A,
-			KeyCode.D, KeyCode.W, KeyCode.S, KeyCode.F, KeyCode.G, inputDispatcher);
+		_registeredPlayers.Add(new PlayerKeyboardInputMapper("1", KeyCode.A,
+			KeyCode.D, KeyCode.W, KeyCode.S, KeyCode.F, KeyCode.G, inputDispatcher));
 	}
 
 
 	public void CheckInput()
 	{
-		_playerZero.CheckInput();
-		_playerOne.CheckInput();
+		foreach (var player in _registeredPlayers)
+		{
+			player.CheckInput();
+		}
+		if (Input.GetKey(KeyCode.P))
+		{
+			int playerNumber = 5;
+			for (int i = 2; i < playerNumber; i++)
+			{
+				_registeredPlayers.Add(new PlayerKeyboardInputMapper(i.ToString(), KeyCode.LeftArrow,
+					KeyCode.RightArrow, KeyCode.UpArrow, KeyCode.DownArrow, KeyCode.RightShift, KeyCode.Return,
+					_inputDispatcher));
+			}
+		}
 	}
 }
--- src/Ggj2020/Assets/Scripts/UI/SplitScreenUI.cs
+++ src/Ggj2020/Assets/Scripts/UI/SplitScreenUI.cs
@@ -5,18 +5,20 @@ using DriverInterface;
 using UnityEngine;
 using UnityEngine.UI;
 using Zenject;
+using Object = UnityEngine.Object;
 
 public class SplitScreenUI : MonoBehaviour
 {
-	public List<ScreenLayout> AvailableLayouts;
-
 	private List<PlayerSplitCam> _activeCameras = new List<PlayerSplitCam>();
+	private List<RawImage> _createdElements = new List<RawImage>();
 	private int _activeNumberOfPlayers;
+	private RectTransform _canvas;
 
 	[Inject]
 	public void Inject(SignalBus bus)
 	{
 		bus.Subscribe<GameSignals.AddPlayerCam>(AddNewPlayerCam);
+		_canvas = GetComponent<RectTransform>();
 	}
 
 	private void AddNewPlayerCam(GameSignals.AddPlayerCam signal)
@@ -28,16 +30,63 @@ public class SplitScreenUI : MonoBehaviour
 
 	private void UpdateScreenLayout()
 	{
-		foreach (var layout in AvailableLayouts)
+		var splits = _activeNumberOfPlayers / 2;
+		var rest = _activeNumberOfPlayers % 2;
+		int leadSide = splits + rest;
+		int supportSide = splits;
+		
+		var screenWidth = _canvas.sizeDelta.x;
+		var screenHeight = _canvas.sizeDelta.y;
+		var mainSplitNumber = _activeNumberOfPlayers > 1 ? 2 : 1;
+		var sideSize = screenWidth / mainSplitNumber ;
+		float leadSidePanelSize = ( screenHeight / leadSide ) ;
+
+		float supportSidePanelSize = 0;
+		if (supportSide > 0)
 		{
-			if (layout.NumberOfPlayers != _activeNumberOfPlayers)
+			supportSidePanelSize = screenHeight / supportSide;
+		}
+
+		int currentPlayerId = 0;
+		int currentPanelId = 0;
+		foreach (var activeCamera in _activeCameras)
+		{
+			activeCamera.TargetCamera.targetTexture = null;
+
+		}
+
+		for (int i = _createdElements.Count-1; i >=0 ; i--)
+		{
+			Destroy(_createdElements[i].gameObject);
+		}
+		_createdElements.Clear();
+		float offset = 0;
+		var slotSize = leadSidePanelSize;
+		foreach (var activeCamera in _activeCameras)
+		{
+			if (currentPlayerId == leadSide)
 			{
-				layout.Disable();
+				slotSize = supportSidePanelSize;
+				offset = sideSize;
+				currentPanelId = 0;
 			}
-			else
-			{
-				layout.Enable(_activeCameras);
-			}
+			var image = new GameObject($"RenderPanelPlayer_{currentPlayerId}").AddComponent<RawImage>();
+			_createdElements.Add(image);
+			image.transform.SetParent(transform);
+			var rectTransform = image.rectTransform;
+			rectTransform.pivot = new Vector2(0, 1);
+			rectTransform.localScale = Vector3.one;
+			rectTransform.sizeDelta = new Vector2(sideSize, slotSize);
+			rectTransform.anchorMin = rectTransform.anchorMax = new Vector2(0, 1);
+			rectTransform.anchoredPosition = new Vector2(offset, -slotSize*currentPanelId);
+			var rt = new RenderTexture((int)rectTransform.sizeDelta.x, (int)rectTransform.sizeDelta.y, 16, RenderTextureFormat.ARGB32);
+			rt.Create();
+			activeCamera.TargetCamera.targetTexture = rt;
+			image.texture = rt;
+			currentPlayerId++;
+			currentPanelId++;
 		}
 	}
+
+	
 }
